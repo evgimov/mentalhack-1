6b57e3fa40d21dc436dada27e219515043b47fd5
rosie.stevenson <rosie.stevenson@tubemogul.com>
2018-02-03 14:24:50 -0800

    adding conf file

--- backend/interact_with_firebase.py
+++ backend/interact_with_firebase.py
@@ -1,9 +1,11 @@
 from firebase import firebase
 from firebase.firebase import FirebaseApplication
 from firebase.firebase import FirebaseAuthentication
+import configparser
 
 if __name__ == '__main__':
     app = FirebaseApplication('https://therapy-finder.firebaseio.com', authentication=None)
-    authentication = FirebaseAuthentication('Anarch1stCat3!','rosalie.m.stevenson@gmail.com', extra={'id': 123})
+    config = configparser.ConfigParser()
+    pwd = config.read('pwd.cfg')
+    authentication = FirebaseAuthentication(pwd,'rosalie.m.stevenson@gmail.com', extra={'id': 123})
     app.authentication = authentication
-    print (authentication.extra)
