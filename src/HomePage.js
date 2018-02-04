897bc62ae59c87b644b085c65bc03f0c570b0502
evgimov <evgimov@gmail.com>
2018-02-04 09:48:36 -0800

    Got routes working

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,16 +5,23 @@ import MyInput from './MyInput';
 import { Grid, Row, Col, Navbar, NavItem, Nav, Panel, Alert, Modal, Button } from 'react-bootstrap';
 import Formsy from 'formsy-react';
 import { Input}  from 'formsy-react-components';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom';
+import TherapySurvey from './TherapySurvey';
+import App from './App';
 
-class EntryPage extends Component {
+class HomePage extends Component {
   constructor(props) {
     super(props);
     this.disableButton = this.disableButton.bind(this);
     this.enableButton = this.enableButton.bind(this);
     this.state = { 			
-			canSubmit: false,
-			patientName: "",
-			patientEmail: ""
+      canSubmit: false
 		};
   }
 
@@ -26,37 +33,40 @@ class EntryPage extends Component {
     this.setState({ canSubmit: true });
   }
 
-	submitForm(data) {
-		let name = data.name;
-		let email = data.email;
-		console.log("Name" + name);
-		console.log("Email" + email);
-		// rest api call
-	};
 
   render() {
     return (
+
         <Grid fluid={true}>
+				
             <Row className="show-grid main-bg">
                 <Col xs={6} md={4} className="centered">
 									<h2>Need help?</h2>
 									<h4>Please, fill out the form</h4>
 									<Formsy onValidSubmit={this.submitForm} onValid={this.enableButton} onInvalid={this.disableButton}>
 										<MyInput
-											name="name"
+                      name="name"
+                      value=""
+                      placeholder="Enter your name"
+                      label="Name"
 										/>
 										<MyInput
 											name="email"
-											validations="isEmail"
-											validationError="This is not a valid email"
+                      value=""
+                      placeholder="Enter your email"
+                      label="Email"
 										/>
-										<button type="submit" disabled={!this.state.canSubmit}>Submit</button>
+                    <Link to={{
+                        pathname: '/survey',
+                      state: { patientName: "Erica", patientEmail: "test@email.com" }
+                      }} ><button type="submit" disabled={!this.state.canSubmit}>Submit</button></Link>
+										
 									</Formsy>
                 </Col>
             </Row>
-	      </Grid>
+	      </Grid>	
     );
   }
 }
 
-export default EntryPage;
+export default HomePage;
